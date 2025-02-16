592df25d938e0e1dbdf10a1e69a479be927fcb6b
RikeshKC1 <pranishkecy102@gmail.com>
2025-02-16 22:13:52 +0545

    pushing where statement learning

---
+++ 2_WHERE_statement.sql
@@ -0,0 +1,12 @@
+SELECT *
+ FROM employee_salary WHERE first_name = 'Ron';
+
+
+SELECT *
+ FROM employee_salary WHERE salary>50000;
+ 
+ 
+SELECT * FROM employee_demographics WHERE gender != 'Female';
+
+SELECT * FROM employee_demographics WHERE birth_date >='1987-01-1';
+
