0b24fbcd75067d348caf5ff6ae700cfc8f121732
RikeshKC1 <pranishkecy102@gmail.com>
2025-02-18 10:14:57 +0545

    pushing files

---
+++ 07_joins.sql
@@ -0,0 +1,12 @@
+-- Intermediate 
+-- joins
+
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+SELECT * FROM parks_and_recreation.employee_salary;
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics 
+INNER JOIN parks_and_recreation.employee_salary ON
+employee_demographics.employee_id = employee_salary.employee_id;
+
