579a668500d71fabce63e2bed0944aabd5ee6c83
RikeshKC1 <pranishkecy102@gmail.com>
2025-02-25 21:10:13 +0545

    String all completed for data analyst

--- 10_String.sql
+++ 10_String.sql
@@ -15,3 +15,23 @@ select trim('      sky       ');
 
 select first_name , lower(first_name) FROM parks_and_recreation.employee_demographics order by 2 ;
 
+
+-- right left 
+select first_name,
+right(first_name,4)
+ from parks_and_recreation.employee_demographics;
+ 
+ select last_name ,
+ left(last_name,4),
+ right(last_name,4),
+ birth_date,
+ substring(birth_date,6,2) as birth_date from parks_and_recreation.employee_demographics;
+ 
+ -- replace
+ select first_name, replace(first_name,'a','z')  from parks_and_recreation.employee_demographics;
+
+-- locate 
+select first_name, locate('an', first_name) from parks_and_recreation.employee_demographics;
+
+-- concat
+select first_name,last_name ,concat(first_name,' ',last_name) from parks_and_recreation.employee_demographics;
